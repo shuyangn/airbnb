c01e256dca4030a8c45058cad7dd153ea3a3273e
shuyangn <shuyangn@DESKTOP-M4U5O1D.localdomain>
2022-08-14 21:49:09 -0400

    add greeting component

---
+++ frontend/components/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Greeting_container from "./greeting/greeting_container";
+
+const App = () => (
+  <div>
+    <header>
+      <h1>Enjoybnb</h1>
+      <Greeting_container />
+    </header>
+  </div>
+);
+
+export default App;
--- frontend/enjoy_bnb.jsx
+++ frontend/enjoy_bnb.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-import {login, signup, logout} from './util/session_api_util';
 import configureStore from './store/store';
+import Root from "./components/root";
 
 document.addEventListener("DOMContentLoaded", () => {
-  const root = document.getElementById("root");
-  ReactDOM.render(<h1>Welcome to EnjoyBnB</h1>, root);
-
   const store = configureStore();
+
+  const root = document.getElementById("root");
+  ReactDOM.render(<Root store = {store}/>, root);
+
   window.getState = store.getState;
   window.dispatch = store.dispatch;
 });
