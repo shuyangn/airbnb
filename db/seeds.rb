b4ae449fddace7d62cf22bece38fe8aebc380bda
shuyangn <shuyangn@DESKTOP-M4U5O1D.localdomain>
2022-08-18 10:52:44 -0400

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+User.destroy_all
+
+demo_user = User.create!(username:'Guest', email: 'guest@email.com', password:'112233')
