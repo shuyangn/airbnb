8d43e7dc3b7becbcc6f1a853bfbedc191b12084f
shuyangn <my email address used on my git repo>
2022-09-18 02:37:22 -0400

    revise splash

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -2,13 +2,21 @@ import React from 'react';
 import NavBarContainer from '../nav_bar/nav_bar_container';
 import ModalContainer from '../modal/modal';
 import SearchBar from '../search/search_bar';
+import { Link } from "react-router-dom";
+
 class Splash extends React.Component {
     render () {
       return (
         <div className="dashboard-background">
           <NavBarContainer />
           <ModalContainer />
-          <SearchBar />
+          {/* <SearchBar /> */}
+          <div className='quote'>
+            <p>"I haven't been everywhere, but it's on my list."</p>
+          </div>
+          <Link to = '/rooms'>
+          <button className='splash-entry-button'><p>expore your next trip</p></button>
+          </Link>
         </div>
 
       );
